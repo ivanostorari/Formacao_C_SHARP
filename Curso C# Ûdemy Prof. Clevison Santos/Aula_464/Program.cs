9303944ce02c22e45e5a94221bb63884ed82fd82
ivanostorari <programadordev41@gmail.com>
2024-08-29 16:21:39 -0400

    first commit

---
+++ Curso C# Ûdemy Prof. Clevison Santos/Aula_464/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; //Importa a namespace System, que fornece as funcionalidade básicas do programa
+using System.Collections.Generic;//Classes grnericas que suportam as coleções
+using System.Linq;//Fornece as classes e interfaces para concuslta LINQ(Language-Integrate Query)
+using System.Text;//Trabalhar com textos e carateres
+using System.Threading.Tasks;//Trabalhar com tarefas
+
+//namespace - Organiza / Define o escopo do programa
+namespace Aula_464//Define o namespace do programa
+{
+    internal class Program// Define a classe principal
+    {
+        static void Main(string[] args)//Define o ponto de entrada
+        {
+            //Exibe a mensagem no console
+            Console.WriteLine("Olá, mundo!");
+
+            Console.WriteLine("\nEstou aprendendo \nC#");
+
+            //Aguarda o usuário encerrar o programa
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Curso C# Ûdemy Prof. Clevison Santos/Aula_465/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_465
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pedir ao usuário para digitar seu nome
+            Console.WriteLine("Digite seu nome:");
+
+            //Cria uma variável chamada nome que vai ler o que o usuário digitar
+            String nome = Console.ReadLine();
+
+            //Exibe a mensagem de saudação
+            Console.WriteLine("\nOlá, " + nome + "!");
+            
+            //Pedir para o usuário digitar a idade.
+            Console.WriteLine("\nDigite sua idade: ");
+
+            //Lê a entrada deo usuário e converte para um número inteiro
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            //Exibe a mensagem de saudação
+            Console.WriteLine("\nOlá, " + nome + "! Você tem " + idade + " anos.");
+            
+            //Augarda o usuário pressionar uma tecla para sair
+            Console.ReadKey();
+        }
+    }
+}
